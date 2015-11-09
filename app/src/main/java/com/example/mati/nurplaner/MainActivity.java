1236913cdf11cf2935d8cac82ca05d669974a5fa
MateuszRajmundKukawski <mrkukawski@gmail.com>
2015-11-09 22:45:26 +0100

    a kurwa mać, co to za pojebany jezyk. sac na ukonczeniu

--- app/src/main/java/com/example/mati/nurplaner/MainActivity.java
+++ app/src/main/java/com/example/mati/nurplaner/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.mati.nurplaner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 
+{
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button);
+        button1.setOnClickListener(this);
+
+    }
+    public void onClickButton1()
+    {
+        startActivity(new Intent("com.example.mati.nurplaner.Sac"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case (R.id.button):
+                onClickButton1();
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/mati/nurplaner/Sac.java
@@ -0,0 +1,129 @@
+package com.example.mati.nurplaner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+public class Sac extends AppCompatActivity implements OnItemSelectedListener, View.OnClickListener {
+
+    Button calcButton;
+    int cylinder;
+    int startP;
+    int endP ;
+    int avgD ;
+    int dTime;
+//    EditText text;
+    EditText startP_ET;
+    EditText endP_ET;
+    EditText avgD_ET;
+    EditText dTime_ET;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sac);
+        calcButton = (Button) findViewById(R.id.caclSacBt);
+        calcButton.setOnClickListener(this);
+        startP_ET = (EditText)findViewById(R.id.startP_TexEdit);
+        endP_ET = (EditText)findViewById(R.id.endP_TexEdit);
+        avgD_ET = (EditText)findViewById(R.id.avgD_TexEdit);
+        dTime_ET = (EditText)findViewById(R.id.timeD_TexEdit);
+
+        Spinner vCylListSpinner = (Spinner) findViewById(R.id.cilinderList);
+
+        vCylListSpinner.setOnItemSelectedListener(this);
+
+        List<String> cylinsers = new ArrayList<String>();
+        cylinsers.add("11");
+        cylinsers.add("12");
+        cylinsers.add("18");
+        cylinsers.add("20");
+        cylinsers.add("24");
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cylinsers);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        vCylListSpinner.setAdapter(dataAdapter);
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
+
+    public void onClickCalcBt()
+    {
+//        String tmp = startP_ET.getText().toString();
+        if (isEmpty(startP_ET))
+        {
+          Toast.makeText(getApplicationContext(), "PODAJ CISNIENIE POCZATKOWE", Toast.LENGTH_LONG).show();
+
+//        startP = Integer.parseInt(startP_ET.getText().toString());
+        }
+        else
+        {
+            startP = Integer.parseInt(startP_ET.getText().toString());
+//            startP = 0;
+        }
+
+        endP = Integer.parseInt(endP_ET.getText().toString());
+        avgD = Integer.parseInt(avgD_ET.getText().toString());
+        dTime = Integer.parseInt(dTime_ET.getText().toString());
+
+            int mySac = getSac(cylinder, startP, endP, dTime, avgD);
+            Toast.makeText(getApplicationContext(), "SAC: " + Integer.toString(mySac), Toast.LENGTH_LONG).show();
+
+    }
+
+
+    public int getSac(int vCyl, int pStart, int pEnd, int dTime, int avgD)
+    {
+        int sac;
+        sac = ((pStart - pEnd) * vCyl/dTime)/(1+avgD/10);
+        return sac;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Butla: " + item, Toast.LENGTH_LONG).show();
+        cylinder = Integer.parseInt(item);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case (R.id.caclSacBt):
+                onClickCalcBt();
+                break;
+        }
+
+    }
+}
